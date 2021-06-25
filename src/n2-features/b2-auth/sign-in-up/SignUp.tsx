233ddeac278af4829a70ef46270bb917e472cc5e
Smithpovvi <vbairash@gmail.com>
2021-06-25 21:49:42 +0300

    Add jsx to sign up

--- src/n2-features/b2-auth/sign-in-up/SignUp.tsx
+++ src/n2-features/b2-auth/sign-in-up/SignUp.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import SuperButton from "../../../n3-MySuperComponents/SuperButton/SuperButton";
+import styles from "./SignUp.module.css"
 
 const SignUp = () => {
-//моя фича
+//Влад
     return(
-        <div>
-            <SuperButton/>
-            Sign UP HERE
+        <div className={styles.inputBlock}>
+            <input placeholder="email:"/>
+            <input placeholder="password:"/>
+            <input placeholder="confirm password:"/>
+            <SuperButton/>     
         </div>
     )
 }
