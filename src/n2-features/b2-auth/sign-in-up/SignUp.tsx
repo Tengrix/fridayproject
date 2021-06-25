48f9454b0fe2cd52f2c63a8a7065f59b5337bdd1
Smithpovvi <vbairash@gmail.com>
2021-06-25 21:22:53 +0300

    Sign up - start development

--- src/n2-features/b2-auth/sign-in-up/SignUp.tsx
+++ src/n2-features/b2-auth/sign-in-up/SignUp.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SuperButton from "../../../n3-MySuperComponents/SuperButton/SuperButton";
 
 const SignUp = () => {
-
+//моя фича
     return(
         <div>
             <SuperButton/>
