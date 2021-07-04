95bbcc5abf1c50c418eee1d645a7f65f83426cc7
Smithpovvi <vbairash@gmail.com>
2021-07-04 17:51:01 +0300

    Finished dev renewallPass

--- src/n2-features/b3-pass/Password/NewPass.tsx
+++ src/n2-features/b3-pass/Password/NewPass.tsx
@@ -17,7 +17,7 @@ const NewPass = () => {
     )
     const commonError = useSelector<AppRootStateType, string>((state) => state.auth.commonError)
     const dispatch = useDispatch()
-    const {token} = useParams<{ token: string }>()
+    const { token } = useParams<{ token: string }>()
     const formik = useFormik({
         initialValues: {
             password: "",
--- src/n2-features/b3-pass/Password/RenewalPass.tsx
+++ src/n2-features/b3-pass/Password/RenewalPass.tsx
@@ -1,23 +1,33 @@
 import { useFormik } from "formik"
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { Redirect } from "react-router-dom"
-import { PATH } from "../../../n1-main/a1-ui/routes/Routes"
 import { forgot } from "../../../n1-main/a2-bll/store/mainAuthReducer"
 import { AppRootStateType } from "../../../n1-main/a2-bll/store/store"
-import SuperInputText from "../../../n3-MySuperComponents/SuperInputText/SuperInputText"
 import styles from "../../../utils/styles/CommonStylesForAuth.module.css"
+
+type FormikErrorType = {
+    email?: string
+}
 
 const RenewalPass = () => {
     const message = `<div style="background-color: lime; padding: 15px"> 
-    password recovery link: <a href='https://Tengrix.github.io/fridayproject/#/new-pass/$token$'>Click to here</a></div>`
+    password recovery link: <a href='https://tengrix.github.io/fridayproject/#/new-pass/$token$'>Click to here</a></div>`
     const fogot = useSelector<AppRootStateType, boolean>((state) => state.auth.fogot)
+    const commonError = useSelector<AppRootStateType, string>((state) => state.auth.commonError)
     const dispatch = useDispatch()
     const formik = useFormik({
         initialValues: {
             email: "",
         },
-        validate: (values) => {},
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = "Email required"
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = "Invalid email address"
+            }
+            return errors
+        },
         onSubmit: (values) => {
             dispatch(forgot(values.email, message))
             formik.resetForm()
@@ -31,14 +41,16 @@ const RenewalPass = () => {
         )
     return (
         <div className={styles.authBlock}>
-            <form onSubmit={formik.handleSubmit}>
+            <form onSubmit={formik.handleSubmit} className={styles.inputBlock}>
                 <input
                     placeholder="email"
                     name="email"
                     onChange={formik.handleChange}
                     value={formik.values.email}
                 />
-                <button type="submit">Enter</button>
+                <button type="submit">Send message</button>
+                {formik.errors.email && <div style={{ color: "red" }}>{formik.errors.email}</div>}
+                {commonError && <div style={{ color: "red" }}>{commonError}</div>}
             </form>
         </div>
     )
