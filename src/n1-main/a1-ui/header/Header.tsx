10fb7abd561a6c9f51311890787d6b7c1ca87557
Smithpovvi <vbairash@gmail.com>
2021-07-08 14:36:25 +0300

    control commit

--- src/n1-main/a1-ui/header/Header.tsx
+++ src/n1-main/a1-ui/header/Header.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 import { NavLink } from "react-router-dom"
 import s from "./Header.module.css"
@@ -6,92 +5,54 @@ import { PATH } from "../routes/Routes"
 import { useSelector } from "react-redux"
 import { AppRootStateType } from "../../a2-bll/store/store"
 import Logout from "../../../n2-features/b2-auth/sign-in-up/Logout"
-
-function Header() {
-    const isLogged = useSelector<AppRootStateType, boolean>((state) => state.auth.isLogged)
-    return (
-        <nav className={s.nav}>
-            <span className={s.logOut}>{isLogged && <Logout />}</span>
-            {isLogged && (
-                <div className={s.item}>
-                    <NavLink to={PATH.UPDATE_USER} activeClassName={s.activeLink} replace>
-                        {" "}
-                        Settings{" "}
-                    </NavLink>
-                </div>
-            )}
-            {isLogged && <div className={s.item}>
-                <NavLink to={PATH.PROFILE} activeClassName={s.activeLink} replace>
-                    {" "}
-                    My Profile{" "}
-                </NavLink>
-            </div>}
-            {!isLogged && <div className={s.item}>
-                <NavLink to={PATH.SIGN_IN} activeClassName={s.activeLink} replace>
-                    sign-in
-                </NavLink>
-            </div>}
-            {!isLogged && <div className={s.item}>
-                <NavLink to={PATH.SIGN_UP} activeClassName={s.activeLink} replace>
-                    sign-up
-                </NavLink>
-            </div>}
-            {/* <div className={s.item}>
-                <NavLink to={PATH.NEW_PASS} activeClassName={s.activeLink} replace>new Pass</NavLink>
-            </div> */}
-            {!isLogged && <div className={s.item}>
-                <NavLink to={PATH.RENEWAL_PASS} activeClassName={s.activeLink} replace>
-                    Get the new Pass
-                </NavLink>
-            </div>}
-            {isLogged && <div className={s.item}>
-                <NavLink to={PATH.PACKS} activeClassName={s.activeLink} replace>
-                    Packs
-                </NavLink>
-            </div>}
-            {isLogged && <div className={s.item}>
-                <NavLink to={PATH.CARDS} activeClassName={s.activeLink} replace>
-                    Cards
-                </NavLink>
-            </div>}
-        </nav>
-=======
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import s from './Header.module.css'
-import {PATH} from "../routes/Routes";
-import {useSelector} from "react-redux";
-import {AppRootStateType} from "../../a2-bll/store/store";
-import Logout from '../../../n2-features/b2-auth/sign-in-up/Logout';
-import { Avatar, Button } from '@material-ui/core';
-import PermIdentityIcon from '@material-ui/icons/PermIdentity';
-import SettingsIcon from '@material-ui/icons/Settings';
-import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import { Avatar, Button } from "@material-ui/core"
+import PermIdentityIcon from "@material-ui/icons/PermIdentity"
+import SettingsIcon from "@material-ui/icons/Settings"
+import VpnKeyIcon from "@material-ui/icons/VpnKey"
 
 function Header() {
     const isLogged = useSelector<AppRootStateType, boolean>((state) => state.auth.isLogged)
 
     return (
         <>
-        {isLogged && <nav className={s.nav}>
-        <div>
-            <NavLink to={PATH.PROFILE} replace><Avatar><PermIdentityIcon/></Avatar></NavLink>
-        </div>
-       <div className={s.headerLink}>
-       <div className={s.item}>
-            <NavLink to={PATH.PROFILE} activeClassName={s.activeLink} replace><Button variant="outlined" ><PermIdentityIcon/>My Profile </Button></NavLink>
-        </div>        
-        <div className={s.item}>
-            <NavLink to={PATH.NEW_PASS} activeClassName={s.activeLink} replace><Button variant="outlined" ><VpnKeyIcon/>New Password</Button></NavLink>
-        </div>
-         <div className={s.item}>
-            <NavLink to={PATH.UPDATE_USER} activeClassName={s.activeLink} replace><Button variant="outlined"><SettingsIcon/> Settings </Button></NavLink>
-        </div>
-        <Logout/>
-       </div>
-    </nav> }
+            {isLogged && (
+                <nav className={s.nav}>
+                    <div>
+                        <NavLink to={PATH.PROFILE} replace>
+                            <Avatar>
+                                <PermIdentityIcon />
+                            </Avatar>
+                        </NavLink>
+                    </div>
+                    <div className={s.headerLink}>
+                        <div className={s.item}>
+                            <NavLink to={PATH.PROFILE} activeClassName={s.activeLink} replace>
+                                <Button variant="outlined">
+                                    <PermIdentityIcon />
+                                    My Profile{" "}
+                                </Button>
+                            </NavLink>
+                        </div>
+                        <div className={s.item}>
+                            <NavLink to={PATH.NEW_PASS} activeClassName={s.activeLink} replace>
+                                <Button variant="outlined">
+                                    <VpnKeyIcon />
+                                    New Password
+                                </Button>
+                            </NavLink>
+                        </div>
+                        <div className={s.item}>
+                            <NavLink to={PATH.UPDATE_USER} activeClassName={s.activeLink} replace>
+                                <Button variant="outlined">
+                                    <SettingsIcon /> Settings{" "}
+                                </Button>
+                            </NavLink>
+                        </div>
+                        <Logout />
+                    </div>
+                </nav>
+            )}
         </>
->>>>>>> origin/master
     )
 }
 
--- src/n2-features/profile/Profile.tsx
+++ src/n2-features/profile/Profile.tsx
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { AppRootStateType } from "../../n1-main/a2-bll/store/store"
 import { GetUserTC } from "../../n1-main/a2-bll/store/mainAuthReducer"
 import { Redirect } from "react-router-dom"
 import { PATH } from "../../n1-main/a1-ui/routes/Routes"
-import styles from "../../utils/styles/CommonStylesForAuth.module.css"
-=======
-import React, {useEffect} from 'react'
-import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType} from "../../n1-main/a2-bll/store/store";
-import {GetUserTC} from "../../n1-main/a2-bll/store/mainAuthReducer";
-import {Redirect} from "react-router-dom";
-import {PATH} from "../../n1-main/a1-ui/routes/Routes";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import s from './Profile.module.css'
->>>>>>> origin/master
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
+import s from "./Profile.module.css"
 
 const Profile = () => {
     const dispatch = useDispatch()
@@ -38,48 +28,32 @@ const Profile = () => {
     if (!isLogged) {
         return <Redirect to={PATH.SIGN_IN} />
     }
-<<<<<<< HEAD
+
     return (
-        <div>
-            <div>
-                <img src={userAvatar} alt="" style={{ height: "300px", width: "300px" }} />
-            </div>
-            <div>{userName}</div>
-            <div>{userEmail}</div>
-        </div>
+        <Card className={s.root}>
+            <CardActionArea>
+                <CardMedia className={s.media} image={userAvatar} title="Contemplative Reptile" />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Name: {userName}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Email: {userEmail}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magnam
+                        veritatis suscipit. Dolor, quas iste eveniet et saepe quibusdam expedita
+                        nihil incidunt quo odio cum culpa, delectus aliquam earum nemo.
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Learn More
+                </Button>
+            </CardActions>
+        </Card>
     )
 }
-export default Profile
-=======
-    
-  return (
-    <Card className={s.root}>
-      <CardActionArea>
-        <CardMedia
-          className={s.media}
-          image={userAvatar}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Name: {userName}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2">
-            Email: {userEmail}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magnam veritatis suscipit. Dolor, quas iste eveniet et saepe quibusdam expedita nihil incidunt quo odio cum culpa, delectus aliquam earum nemo.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <Button size="small" color="primary">
-          Learn More
-        </Button>
-    </CardActions>
-    </Card>
-  );
-}
 
-export default Profile;
->>>>>>> origin/master
+export default Profile
